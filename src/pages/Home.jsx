12fbd0d47e8c23982d212e3ceb35c6a5843527e9
0xyusufz <gurucoc471@gmail.com>
2026-02-27 12:33:04 +0530

    home page added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
-
+import {Box,Image,Text} from '@chakra-ui/react'
+import btc from '../assets/btc.png'
 const Home = () => {
   return (
-    <div>
-      <h1>home page</h1>
-    </div>
+    <Box bgColor={"blackAlpha.900"} w={"full"} h={"85vh"}>
+
+
+    <Image w={"full"} h={"full"} objectFit={"contain"} src={btc} filter={"grayscale(.5)"}/>
+
+    <Text fontSize={["4xl","6xl"]} textAlign={"center"} fontweight={"thin"} color={"white"} mt={"-20"}>Crypto Space</Text>
+
+
+    </Box>
+
   )
 }
 
